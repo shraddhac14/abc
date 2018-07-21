899ae65f40d8201d846b4e6ae181495d7a11bccc
shraddhac14 <2016shraddha.chavan@ves.ac.in>
2018-07-21 16:07:22 +0530

    android

---
+++ app/src/main/java/com/example/lenovo/annadhan/SubmitActivity.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.annadhan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubmitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit);
+    }
+}
